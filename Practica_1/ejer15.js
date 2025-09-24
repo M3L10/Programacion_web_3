132a5c59f15c33a4bc237f558f24c753932224dc
meliodas <ronall045@gmail.com>
2025-09-24 11:07:29 -0400

    Agregado archivo ejer15.js en Practica_1

---
+++ Practica_1/ejer15.js
@@ -0,0 +1,19 @@
+//15. Proporcione un ejemplo para convertir un callback en una promesa
+function obtenerDatoConCallback(callback) {
+  setTimeout(() => {
+    callback("Dato obtenido con callback");
+  }, 1000);
+}
+
+// Convertimos el callback en una promesa
+function obtenerDatoConPromesa() {
+  return new Promise((resolve) => {
+    obtenerDatoConCallback((resultado) => {
+      resolve(resultado);
+    });
+  });
+}
+
+obtenerDatoConPromesa().then((dato) => {
+  console.log("Resultado:", dato);
+});
