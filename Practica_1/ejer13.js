d230ce3acf9d0816fb1ebdcbb893d369f6833998
meliodas <ronall045@gmail.com>
2025-09-24 11:06:14 -0400

    Agregado archivo ejer1.js en Practica_1

---
+++ Practica_1/ejer13.js
@@ -0,0 +1,46 @@
+//13. Proporcione un ejemplo concreto donde el anidamiento de promesas se puede
+//reescribir mejor con async/await haciendo el código más limpio y mantenible.
+/*function obtenerSaludo() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hola");
+    }, 2000);
+  });
+}
+
+function agregarNombre(saludo) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(saludo + ", Juan");
+    }, 2000);
+  });
+}
+
+obtenerSaludo().then((saludo) => {
+  agregarNombre(saludo).then((mensajeFinal) => {
+    console.log(mensajeFinal);
+  });
+});*/
+function obtenerSaludo() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hola");
+    }, 1000);
+  });
+}
+
+function agregarNombre(saludo) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(saludo + ", Kakaroto");
+    }, 2000);
+  });
+}
+
+async function saludar() {
+  const saludo = await obtenerSaludo();
+  const mensajeFinal = await agregarNombre(saludo);
+  console.log(mensajeFinal);
+}
+
+saludar();
