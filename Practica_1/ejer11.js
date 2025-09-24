66cb8937ea1f53040db823ea1effbc3f9351926d
meliodas <ronall045@gmail.com>
2025-09-24 11:05:26 -0400

    Agregado archivo ejer11.js en Practica_1

---
+++ Practica_1/ejer11.js
@@ -0,0 +1,15 @@
+//11. Proporcione un ejemplo concreto de encadenamiento de promesas.
+const promesa1 = new Promise((resolve) => {
+    setTimeout(() =>{
+        resolve(8)
+    }, 2000);
+});
+promesa1.then((numero) => {
+    console.log("Número inicial:", numero);
+    return numero * 2;
+}).then((resultado) => {
+     console.log("Número después de multiplicar por 2:", resultado);
+    return resultado + 3; 
+ }).then((final) => {
+    console.log("Número final después de sumar 3:", final);
+ });
