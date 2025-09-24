4b383c49fc6ea67bfb4dbe1900a107e0959835d0
meliodas <ronall045@gmail.com>
2025-09-24 11:00:38 -0400

    Agregado archivo ejer6.js en Practica_1

---
+++ Practica_1/ejer6.js
@@ -0,0 +1,8 @@
+//6. Tomar los dos primeros elementos de un arreglo y almacenarlos en dos variables
+//mediante desestructuración.
+const numeros = [543, 00, 30, 40, 40];
+
+let [primero, segundo] = numeros;
+
+console.log("Primer elemento:", primero); 
+console.log("Segundo elemento:", segundo);
